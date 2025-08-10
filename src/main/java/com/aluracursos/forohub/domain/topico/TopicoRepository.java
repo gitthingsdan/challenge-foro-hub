20f01a7c33626e329ef97c290cafb2af73765a07
gtd <dvillarroela@pm.me>
2025-08-09 22:30:05 -0400

    Eliminar importaciones innecesarias en TopicoRepository

--- src/main/java/com/aluracursos/forohub/domain/topico/TopicoRepository.java
+++ src/main/java/com/aluracursos/forohub/domain/topico/TopicoRepository.java
@@ -2,8 +2,6 @@ package com.aluracursos.forohub.domain.topico;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface TopicoRepository extends JpaRepository<Topico, Long> {
 	boolean existsByTituloAndMensaje(String titulo, String mensaje);
 
