f98dfc3684cda002f60f8541411f66327e6a38ce
gtd <dvillarroela@pm.me>
2025-08-09 22:26:26 -0400

    Añadir clases de dominio Topico y Status con operaciones básicas

---
+++ src/main/java/com/aluracursos/forohub/domain/topico/Status.java
@@ -0,0 +1,7 @@
+package com.aluracursos.forohub.domain.topico;
+
+public enum Status {
+	SIN_RESPUESTA,
+	RESUELTO,
+	SPAM
+}
---
+++ src/main/java/com/aluracursos/forohub/domain/topico/Topico.java
@@ -0,0 +1,47 @@
+package com.aluracursos.forohub.domain.topico;
+
+import jakarta.persistence.*;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Table(name = "topicos")
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Topico {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String titulo;
+	private String mensaje;
+	private LocalDateTime fechaDeCreacion;
+	@Enumerated(EnumType.STRING)
+	private Status status;
+	private String autor;
+	private String curso;
+
+	public Topico(DatosCreacionTopico datos) {
+		this.id = null;
+		this.titulo = datos.titulo();
+		this.mensaje = datos.mensaje();
+		this.fechaDeCreacion = LocalDateTime.now();
+		this.status = Status.SIN_RESPUESTA;
+		this.autor = datos.autor();
+		this.curso = datos.curso();
+	}
+
+	public void actualizar(DatosActualizacionTopico datos) {
+		if (datos.titulo() != null) this.titulo = datos.titulo();
+		if (datos.mensaje() != null) this.mensaje = datos.mensaje();
+		if (datos.status() != null) this.status = datos.status();
+		if (datos.autor() != null) this.autor = datos.autor();
+		if (datos.curso() != null) this.curso = datos.curso();
+	}
+}
