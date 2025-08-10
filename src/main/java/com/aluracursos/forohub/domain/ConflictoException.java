8a67b10b856b0eb4c976a3b3e7df658552c861ed
gtd <dvillarroela@pm.me>
2025-08-09 22:30:29 -0400

    Añadir clases de excepción para manejo de validaciones en el dominio

---
+++ src/main/java/com/aluracursos/forohub/domain/ConflictoException.java
@@ -0,0 +1,7 @@
+package com.aluracursos.forohub.domain;
+
+public class ConflictoException extends ValidacionException {
+	public ConflictoException(String message) {
+		super(message);
+	}
+}
---
+++ src/main/java/com/aluracursos/forohub/domain/ValidacionException.java
@@ -0,0 +1,7 @@
+package com.aluracursos.forohub.domain;
+
+public class ValidacionException extends RuntimeException {
+	public ValidacionException(String message) {
+		super(message);
+	}
+}
